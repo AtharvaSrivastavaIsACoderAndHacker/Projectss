b56c660c2d7258eeadcb17839d1e440e7365e50a
AtharvaSrivastavaIsACoderAndHacker <122040797+AtharvaSrivastavaIsACoderAndHacker@users.noreply.github.com>
2023-01-05 22:32:37 +0530

    Add files via upload

---
+++ Projects/Password/set_password.py
@@ -0,0 +1,11 @@
+# To Apply It On A Program, You Have To Paste This Python File Into Your Project Folder Right Below Your Python Program And Type "import set_password.py"(excluding apostrophies) Into The Program Which You Want. Samjheeee????
+
+password = "god_promise_ye_kaam_karega"
+print ("Welcome, enter password to login")
+attempt = input()
+
+if attempt == password:
+    print("You have been successfully loged on the server")
+else:
+    print("Galat Password !!! Bhag ja yahan se")
+    exit();
