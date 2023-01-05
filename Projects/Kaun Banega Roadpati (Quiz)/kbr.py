df3433f3aa6437698fc941b93aeab321aa844026
AtharvaSrivastavaIsACoderAndHacker <122040797+AtharvaSrivastavaIsACoderAndHacker@users.noreply.github.com>
2023-01-05 22:30:43 +0530

    Add files via upload

---
+++ Projects/Kaun Banega Roadpati (Quiz)/kbr.py
@@ -0,0 +1,185 @@
+
+print ("Note: First Letter Should Be Capital.")
+
+
+# start
+print("What Is The Capital Of Arunachal Pradesh ?")
+answer = input()
+
+if (answer == 'Itanagar'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 1")
+# end
+
+
+# start
+print("What Is Illuminati ?")
+answer = input()
+
+if (answer == 'An Organisation'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 2")
+# end
+
+
+# start
+print("What Was The Cause For Titanic To Sink ?")
+answer = input()
+
+if (answer == 'An Iceberg'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 3")
+# end
+
+
+# start
+print("Who Is Tech Burner ?")
+answer = input()
+
+if (answer == 'Shlok Srivastava'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 4")
+# end
+
+
+# start
+print("Who Is The World's Most Advanced AI Robot ?")
+answer = input()
+
+if (answer == 'Sofia'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 5")
+# end
+
+
+# start
+print("Is 'Creator Insider' YouTube's Official Channel ?")
+answer = input()
+
+if (answer == 'No'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 6")
+# end
+
+
+# start
+print("Where Did Ruskin Bond Live ?")
+answer = input()
+
+if (answer == 'Musoorie'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 7")
+# end
+
+
+# start
+print("Who Invented Light Bulb ?")
+answer = input()
+
+if (answer == 'Thomas Edison'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 8")
+# end
+
+
+# start
+print("What Was YouTube's Tagline 11 Years Back ?")
+answer = input()
+
+if (answer == 'Brodcast Yourself'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 9")
+# end
+
+
+# start
+print("What Is 'Pegasus' ?")
+answer = input()
+
+if (answer == 'A Virus'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 10")
+# end
+
+
+# start
+print("Is Anabelle Real ?")
+answer = input()
+
+if (answer == 'No'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 11")
+# end
+
+
+# start
+print("Which 2 News Channel's Combination Is Sansad TV ?")
+answer = input()
+
+if (answer == 'Rajya Sabha, Lok Sabha'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 12")
+# end
+
+
+# start
+print("What Is Full-Form Of HP ?")
+answer = input()
+
+if (answer == 'Hewlett Packard'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 13")
+# end
+
+
+# start
+print("Which Is The Latest Generation Of Intel Processors In 2022 ?")
+answer = input()
+
+if (answer == '12th Generation' ):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 14")
+
+
+# end
+# start
+print("What Is Ciena ?")
+answer = input()
+
+if (answer == 'A Software Development & Server Management Company'):
+    print('Well Done')
+else:
+    print("Wrong Answer")
+    print("Your Score = 15")
+# end
+
+
+print ("Thanks For Playing !!!!!" )
