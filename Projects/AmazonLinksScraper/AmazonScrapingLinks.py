c5e273f5ec4914ef1f96768f497d7af45141662b
AtharvaSrivastavaIsACoderAndHacker <122040797+AtharvaSrivastavaIsACoderAndHacker@users.noreply.github.com>
2023-07-04 06:13:08 +0530

    Add files via upload

---
+++ Projects/AmazonLinksScraper/AmazonScrapingLinks.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from os import system
+
+system("cls")
+
+query = input('Enter The Product To Scrape : ')
+url = f"https://www.amazon.in/s?k={query}"
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+
+links = []
+for link in soup.findAll('a'):
+      linkhref = link.get('href')
+      if linkhref != None:
+            if "/dp/" in linkhref:
+                  links.append(link.get("href"))
+                  
+print(links)
