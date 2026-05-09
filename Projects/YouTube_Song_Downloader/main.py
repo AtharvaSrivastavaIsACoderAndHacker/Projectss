0dcb501c77136dfd0dfe50b32e2e70dfc1052f41
Atharva Srivastava <atharva201004@gmail.com>
2026-05-10 01:53:06 +0530

    10/5/2026 01:49 ++_(just a lazy soln for downloading my spotify 177 playlist offline)

--- Projects/YouTube_Song_Downloader/main.py
+++ Projects/YouTube_Song_Downloader/main.py
@@ -5,26 +5,26 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 def downloadSong(name):
-    system(f"yt-dlp -x --audio-format mp3 --embed-thumbnail --add-metadata \"ytsearch1:{name}\"")
+    system(
+        f'yt-dlp -x --audio-format mp3 --embed-thumbnail --add-metadata "ytsearch1:{name}"'
+    )
 
 
 df = pd.read_csv("~/Music/liked.csv")
-df = (df["Track Name"])
+df = df["Track Name"]
 
-
+# i set the no. of workers to be 16 cause my cpu can handle it, don't blindly put anything in there or you might make ur computer a toaster ! if you have no idea of what ur cpu can handle start with a 2 or 4, or if u have a really potato pc, start with a 1
 with ThreadPoolExecutor(max_workers=16) as executor:
     executor.map(downloadSong, df)
-
-
 
 
 # this is just some shit i needed to download my spotify playlost offline
 # 1. go to exportify and export your playlist to a csv
 # 2. come here and get the name
-# 3. feed the names as search keywords for yt-dlp, so that it downloads the audio from the first search result 
-    #  yt-dlp -x --audio-format mp3 --embed-thumbnail --add-metadata "ytsearch1:kabhi jo badal barse"
+# 3. feed the names as search keywords for yt-dlp, so that it downloads the audio from the first search result
+#  yt-dlp -x --audio-format mp3 --embed-thumbnail --add-metadata "ytsearch1:kabhi jo badal barse"
 #  yeah ik that its not that reliable cause the first search result isnt often the needed one but what do you want ? me to download 177 songs from the internet manually ?
 
 
-
 #  Nothing serious just a utility/hobby/ idiotic project to just get a free commit !
+
